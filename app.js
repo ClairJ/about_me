37d0cfb0736ad41ecad87f33d5606d879858348e
Joel Clair <clair.joel@yahoo.com>
2017-09-19 17:08:02 -0700

    Final version of biograpghy site made in day2 lab

--- app.js
+++ app.js
@@ -1,14 +1,69 @@
 'use strict';
-var userName = prompt("Thanks for stopping by! What is your name?");
+var userName = prompt('Welcome! What is your name?');
+console.log('Username is ' + userName);
 
-alert('Hi, ' + userName + 'see if you can get the right answer to the following questions about me.');
+var question1 = prompt('Alrighty ' + userName + ' what is my all time favorite food?').toLowerCase();
+console.log('A1 is ' + question1)
+if (question1 === 'lasagna') {
+  alert('You would make Garfield proud!');
 
-//question 1
-var answer1 = prompt('Do I have 4 cats?').toLowerCase();
-if (answer1 === 'n' || answer1 === 'no') {
-  //Tell the user they got the answer
-  alert('You are right!');
 } else {
-  //Tell the user they got the wrong answer
-  alert('You are wrong');
+  alert("wow, just wow. You're dead wrong but keep going I guess.");
+}
+
+var question2 = prompt('Sweet ' + userName + ' now, who is my favorite pop star').toLowerCase();
+console.log('A2 is ' + question2)
+if (question2 === 'katy perry') {
+  alert('Do you ever feel, like a plastic bag? flowing through the wind');
+} else {
+  alert('It was Katy Perry... How could you not guess Katy Perry?');
+}
+
+var question3 = prompt("What's the name of my favorite pet?").toLowerCase();
+console.log('A3 ' + question3)
+if (question3 === 'gizmo') {
+  alert('She is the greatest... Yes, she!')
+} else {
+  alert('The Bio is a pretty important page...')
+}
+
+var question4 = prompt('Rick and Morty?').toLowerCase();
+console.log('A4 ' + question4)
+if (question4 === 'yes' || question4 === 'y') {
+  alert('Wubalubadubdub');
+
+} else {
+  alert("Start today, you're missing out");
+}
+
+var question5 = prompt('Do I enjoy long hikes on the horizon?').toLowerCase();
+console.log('A5 ' + question5)
+if (question5 === 'no' || question5 === 'n') {
+  alert("You know me to well and it's starting to get weird");
+} else {
+  alert('Hiking is the last thing I would ever do, even if theres a fire!');
+}
+
+var question6 = prompt('Have I ever seen a camel?').toLowerCase();
+console.log('A6 ' + question6)
+if (question6 === 'no' || question6 === 'n') {
+  alert('Nice, they actually terrify me.');
+} else {
+  alert('Never have and never will they terrify me');
+}
+
+var question7 = prompt('Am I addicted to building computers?').toLowerCase();
+console.log('A7 ' + question7)
+if (question7 === 'yes' || question7 === 'y') {
+  alert('Yes! My apt has banned my packages from being sent to the office');
+} else {
+  alert('It would be highly likely that if were all here for software development that a majority of us would love computers!');
+}
+
+var question8 = prompt('Dogs?').toLowerCase();
+console.log('A8 is ' + question8)
+if (question8 === 'yes' || question8 === 'y') {
+  alert('Gizmo is everything to me, right under lasagna');
+} else {
+  alert("You didn't read the bio");
 }
