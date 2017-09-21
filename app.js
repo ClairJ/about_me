9b1a6059c6b95cf73c06dfb5ca4e4cd79a9397d2
Joel Clair <clair.joel@yahoo.com>
2017-09-20 22:17:57 -0700

    Final changes made to the about_me to fit the criteria

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 var userName = prompt('Welcome! What is your name?');
 console.log('Username is ' + userName);
 
+var score = 0;
+
+console.log('score is ' + score)
 var question1 = prompt('Alrighty ' + userName + ' Is my all time favorite food lasagna?').toLowerCase();
 console.log('A1 is ' + question1)
 if (question1 === 'yes' || question1 === 'y') {
   alert('You would make Garfield proud!');
-
+  score ++;
+  console.log('score is ' + score)
 } else {
   alert("wow, just wow. You're dead wrong but keep going I guess.");
 }
@@ -15,6 +19,8 @@ var question2 = prompt('Sweet ' + userName + ' Do I jam out to Katy Perry?').toL
 console.log('A2 is ' + question2)
 if (question2 === 'yes' || question2 === 'y') {
   alert('Do you ever feel, like a plastic bag? flowing through the wind');
+  score ++;
+  console.log('score is ' + score)
 } else {
   alert('It was Katy Perry... How could you not guess Katy Perry?');
 }
@@ -23,6 +29,8 @@ var question3 = prompt("Is my female dog\'s name Gizmo?").toLowerCase();
 console.log('A3 ' + question3)
 if (question3 === 'yes' || question3 === 'y') {
   alert('She is the greatest... Yes, she!')
+  score ++;
+  console.log('score is ' + score)
 } else {
   alert('The Bio is a pretty important page...')
 }
@@ -31,7 +39,8 @@ var question4 = prompt('Rick and Morty?').toLowerCase();
 console.log('A4 ' + question4)
 if (question4 === 'yes' || question4 === 'y') {
   alert('Wubalubadubdub');
-
+  score ++;
+console.log('score is ' + score)
 } else {
   alert("Start today, you're missing out");
 }
@@ -40,30 +49,83 @@ var question5 = prompt('Do I enjoy long hikes on the horizon?').toLowerCase();
 console.log('A5 ' + question5)
 if (question5 === 'no' || question5 === 'n') {
   alert("You know me to well and it's starting to get weird");
+  score ++;
+  console.log('score is ' + score)
 } else {
   alert('Hiking is the last thing I would ever do, even if theres a fire!');
 }
 
-var question6 = prompt('Have I ever seen a camel?').toLowerCase();
-console.log('A6 ' + question6)
-if (question6 === 'no' || question6 === 'n') {
-  alert('Nice, they actually terrify me.');
-} else {
-  alert('Never have and never will. They terrify me and you\'re starting to as well.');
+
+var age = '21';
+for (var i = 0; i <= 4; i++) {
+  var age = '21';
+  var question6 = prompt('How old am I?').toLowerCase();
+  console.log('A6 ' + question6);
+  if(question6 === age) {
+    alert('Yes, BUT I still have a lot of maturing to do.');
+    i+=4;
+    score++;
+    console.log('score is ' + score)
+  } else if (question6 < '21'){
+    alert('ouch a little higher');
+  } else if (question6 > '21'){
+    alert('bring it back down to the 20 range');
+  }
 }
 
-var question7 = prompt('Am I addicted to building computers?').toLowerCase();
+
+
+
+var country = ['japan', 'usa', 'mexico', 'canada'];
+for (var i = 0; i < 7; i++) {
+  console.log(country);
+  var question7 = prompt('Guess a country I have been to').toLowerCase();
+  console.log('A7 ' + question7);
+
+  if (question7 === country[0] || question7 === country[1] || question7 === country[2] || question7 === country[3]) {
+      alert('Sweet! I\'ve been to Japan, Canada, USA, and Mexico.');
+      i+=7;
+      score++;
+      console.log('score is ' + score)
+  } else {
+      alert('nope');
+
+  }
+}
+
+alert('You got ' + score + ' out of 7!!!!!');
+//Too many yes no questions
+
+/*
+var question7 = prompt('Have I ever seen a camel?').toLowerCase();
 console.log('A7 ' + question7)
-if (question7 === 'yes' || question7 === 'y') {
+if (question6 === 'no' || question6 === 'n') {
+  alert('Nice, they actually terrify me and you\'re starting to as well.');
+  score ++;
+  console.log('score is ' + score)
+} else {
+  alert('Never have and never will. They terrify me.');
+}
+*/
+/*
+var question8 = prompt('Am I addicted to building computers?').toLowerCase();
+console.log('A8 ' + question8)
+if (question8 === 'yes' || question8 === 'y') {
   alert('Yes! My apt has banned my packages from being sent to the office');
+  score ++;
+  console.log('score is ' + score)
 } else {
   alert('It would be highly likely that if were all here for software development that a majority of us would love computers!');
 }
-
-var question8 = prompt('Dogs?').toLowerCase();
-console.log('A8 is ' + question8)
-if (question8 === 'yes' || question8 === 'y') {
+*/
+/*
+var question9 = prompt('Dogs?').toLowerCase();
+console.log('A9 is ' + question9)
+if (question9 === 'yes' || question9 === 'y') {
   alert('Gizmo is everything to me, right under lasagna');
+  score ++;
+  console.log('score is ' + score)
 } else {
   alert("You didn't read the bio");
 }
+*/
